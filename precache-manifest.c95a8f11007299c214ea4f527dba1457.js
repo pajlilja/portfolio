0e730165e968db8b274eeab65e371ef443de1fa1
root <root@DESKTOP-8JBCC93.localdomain>
2019-10-12 12:33:25 +0200

    Updates

---
+++ precache-manifest.c95a8f11007299c214ea4f527dba1457.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7060e4e2f5a93be606d9ee2c08a84594",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "706eec88bf16d1f727c1",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6de79a463fd4bf754153",
+    "url": "/portfolio/static/css/main.42190a99.chunk.css"
+  },
+  {
+    "revision": "706eec88bf16d1f727c1",
+    "url": "/portfolio/static/js/2.fa9fa619.chunk.js"
+  },
+  {
+    "revision": "6de79a463fd4bf754153",
+    "url": "/portfolio/static/js/main.e82d17f8.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "6d8623e64a3837147cfc086602f9187b",
+    "url": "/portfolio/static/media/CV.6d8623e6.pdf"
+  },
+  {
+    "revision": "d06e72dedf7d0cd7f330237f71ac9f6f",
+    "url": "/portfolio/static/media/attack2.d06e72de.png"
+  },
+  {
+    "revision": "1296494da58bce62b33eab44c1449b24",
+    "url": "/portfolio/static/media/chase3.1296494d.png"
+  },
+  {
+    "revision": "157df133c31b3e5926dc83b0cfb5410b",
+    "url": "/portfolio/static/media/deat.157df133.png"
+  },
+  {
+    "revision": "7f09b2ce7dcb7810ebc10ac527d7c123",
+    "url": "/portfolio/static/media/detailpage3.7f09b2ce.png"
+  },
+  {
+    "revision": "5e3458d5ea7cef768d490057acbf143f",
+    "url": "/portfolio/static/media/end.5e3458d5.png"
+  },
+  {
+    "revision": "9e59474d9e2e1bd0d3e57b9ba41a8238",
+    "url": "/portfolio/static/media/episode.9e59474d.png"
+  },
+  {
+    "revision": "8875a60276d5eaf553df2042c72ee09b",
+    "url": "/portfolio/static/media/front.8875a602.png"
+  },
+  {
+    "revision": "cb3739ab652f37bc9026aa6c71b321a8",
+    "url": "/portfolio/static/media/front.cb3739ab.png"
+  },
+  {
+    "revision": "fc8e41880d1cb99fe738f290061a1590",
+    "url": "/portfolio/static/media/game.fc8e4188.png"
+  },
+  {
+    "revision": "131670a8742f0d0bf988cdc324ab2614",
+    "url": "/portfolio/static/media/game2.131670a8.png"
+  },
+  {
+    "revision": "e54e90399dc7d1b4395e2b86e6411da5",
+    "url": "/portfolio/static/media/idle2.e54e9039.png"
+  },
+  {
+    "revision": "68a5026869e82a2db9c1c1cb1d43912a",
+    "url": "/portfolio/static/media/part2.68a50268.png"
+  },
+  {
+    "revision": "ce46c47fa8bf59b0f58427d5d37f9f32",
+    "url": "/portfolio/static/media/plan2.ce46c47f.png"
+  },
+  {
+    "revision": "c69bc0412ed50c99ef332ed5a3b39361",
+    "url": "/portfolio/static/media/slider.c69bc041.png"
+  },
+  {
+    "revision": "2799126d7d22c2660aa5516f0259cf62",
+    "url": "/portfolio/static/media/start.2799126d.png"
+  },
+  {
+    "revision": "c479c555f8f620b108c656ab36bc79a2",
+    "url": "/portfolio/static/media/start.c479c555.png"
+  }
+]);
